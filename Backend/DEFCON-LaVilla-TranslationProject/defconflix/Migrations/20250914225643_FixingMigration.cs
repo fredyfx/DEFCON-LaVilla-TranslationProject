b68e51a9702f2c245cac4c5dee6f216dec2ff203
Fredy Guibert <fredy_fx@outlook.com>
2025-09-14 15:58:11 -0700

    bugFix. Fixing Migration realted to user role by default.

---
+++ Backend/DEFCON-LaVilla-TranslationProject/defconflix/Migrations/20250914225643_FixingMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace defconflix.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 2,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+        }
+    }
+}
